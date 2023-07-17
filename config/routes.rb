16c3e40322e99d2e160fe7e5115389230d858843
Sarah Garlock <garlock.sarah10@gmail.com>
2023-07-17 15:49:25 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,15 @@ Rails.application.routes.draw do
   # root 'articles#index"
 
   root 'application#welcome'
+  get '/register', to: 'users#new', as: 'register'
+  post '/register', to: 'users#create'
 
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:id/discover', to: 'movies#discover', as: 'discover'
-  get '/register', to: 'users#new', as: 'register'
   
   get '/users/:id/movies', to: 'movies#index', as: 'movies'
-  get '/users/:user_id/movies/:movie_id', to: 'movies#show', as: 'movie_show'
 
+  get '/users/:user_id/movies/:movie_id', to: 'movies#show', as: 'movie_show'
   get '/users/:user_id/movies/:movie_id/viewing-party/new', to: 'viewing_parties#new', as: 'new_viewing_party' 
   post '/users/:user_id/movies/:movie_id/viewing-party/new', to: 'viewing_parties#create', as: 'create_viewing_party'
 
@@ -20,5 +21,7 @@ Rails.application.routes.draw do
 
   # post '/search', to: 'search#search'
 
-  resources :users, only: [:create]
+  resources :users, only: [:new, :create, :show] do
+    
+  end
 end
