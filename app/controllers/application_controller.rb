dc8c1ba9d2f4edbb6d50612b46e8786c0af1ec75
Sarah Garlock <garlock.sarah10@gmail.com>
2023-07-18 15:43:21 -0600

    checks is user is signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :user_signed_in?
+
   def welcome
-    @users = User.all
+    @users = user_signed_in? ? User.all : []
   end
 
   def current_user
     @_current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def user_signed_in?
+    current_user.present?
+  end
+  
 end
